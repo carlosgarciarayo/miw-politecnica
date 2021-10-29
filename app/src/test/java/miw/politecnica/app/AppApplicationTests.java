b4ae26f96afff16119c8fb4d72d3ce3e79aab5f8
carlos <carlos>
2021-10-29 10:23:53 +0200

    first commit

---
+++ app/src/test/java/miw/politecnica/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package miw.politecnica.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
